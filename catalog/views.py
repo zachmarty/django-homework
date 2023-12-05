4ca7ebada12d4c08cb2d3754236a513bb36ea3f3
Pavel <pashapasha-2013@mail.ru>
2023-12-06 00:21:35 +0300

    small fix

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from catalog.models import Product
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 from django.urls import reverse, reverse_lazy
 from django.views.generic.detail import SingleObjectMixin
 from django.views import View
@@ -14,37 +20,44 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "catalog/product.html"
 
+
 class ProductCreateView(CreateView):
     model = Product
-    fields = ('name', 'description', 'image', 'category', 'price')
-    success_url = reverse_lazy('catalog:index')
+    fields = ("name", "description", "image", "category", "price")
+    success_url = reverse_lazy("catalog:index")
+
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ('name', 'description', 'image', 'category', 'price')
+    fields = ("name", "description", "image", "category", "price")
 
     def get_success_url(self):
-        return reverse('catalog:view', args=[self.kwargs.get('pk')])
+        return reverse("catalog:view", args=[self.kwargs.get("pk")])
+
 
 class ProductDetailView(DetailView):
     model = Product
 
+
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('catalog:index')
+    success_url = reverse_lazy("catalog:index")
+
 
 # Create your views here.
 
 
 class ProductContacts(SingleObjectMixin, View):
     model = Product
-    
+    http_method_names = ["get"]
+
     def get(self, request, *args, **kwards):
-        return render(request, 'catalog/contacts.html')
+        return render(request, "catalog/contacts.html")
+
 
 class ProductThanks(SingleObjectMixin, View):
     model = Product
+    http_method_names = ["get"]
 
     def post(self, request, *args, **kwards):
-        return render(request, 'catalog/thanks.html')
-
+        return render(request, "catalog/thanks.html")
