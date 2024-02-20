5df366f88da2b739c58a2ce562eb15b5daeda3f9
Pavel <pashapasha-2013@mail.ru>
2024-02-20 13:36:54 +0300

    verification

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, VerifyView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("logout", LogoutView.as_view(), name="logout"),
     path("register", RegisterView.as_view(), name="register"),
     path("profile", ProfileView.as_view(), name="profile"),
+    path("verify", VerifyView.as_view(), name="verify"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.db.models.base import Model as Model
+from django.forms import ValidationError
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, TemplateView
@@ -23,15 +24,29 @@ class RegisterView(CreateView):
         
         send_mail(
             subject="Приветствуем на нашем сайте",
-            message=f"Вы успешно зарегестрировались",
+            message=f"Вы успешно зарегестрировались, для завершения авторизации, введите данный код {new_user.token}",
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[new_user.email],
         )
-        return redirect('users.views.VerifyView')   
+        return redirect('/users/verify', kwargs={"user":new_user})   
 
 
-class VerifyViewView(TemplateView):
+class VerifyView(TemplateView):
     template_name = 'users/verify.html'
+    success_url = reverse_lazy("users:profile")
+    
+    def form_valid(self, form):
+        
+        user = User.objects.get(verify_key=self.kwargs["user"])
+        if form.data['key'] == user.verify_key:
+            user.is_active = True
+            user.save()    
+            return super().form_valid(form)
+        else:
+            raise ValidationError(form.data['key'].error_message['invalid'])
+    
+    
+    
 
 
 class ProfileView(UpdateView):
